6279582cef90c53e1de1ea6b19ab0652eb5215e2
Gavin.Xue <jlxuebailong@hotmal.com>
2018-08-03 16:54:55 +0800

    sm2 lab

---
+++ security/src/main/java/com/citic/cipher/sm/MyKeyPair.java
@@ -0,0 +1,19 @@
+package com.citic.cipher.sm;
+
+public class MyKeyPair {
+	private String privateKey;
+	private String publicKey;
+	public MyKeyPair(String privateKey, String publicKey) {
+		this.privateKey = privateKey;
+		this.publicKey = publicKey;
+	}
+	public String getPrivateKey() {
+		return privateKey;
+	}
+	public String getPublicKey() {
+		return publicKey;
+	}
+	
+	
+
+}
