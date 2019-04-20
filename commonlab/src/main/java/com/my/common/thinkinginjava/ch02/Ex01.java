0e4d91aa4171c2d07126a7391c77395e724b92fe
jlxuebailong <jlxuebailong@hotmail.com>
2019-04-20 15:57:23 +0800

    lab

---
+++ commonlab/src/main/java/com/my/common/thinkinginjava/ch02/Ex01.java
@@ -0,0 +1,14 @@
+package com.my.common.thinkinginjava.ch02;
+
+/**
+ * Created by gavin on 2019/4/20.
+ */
+public class Ex01 {
+    private int i;
+    private char c;
+    public static void main(String[] args){
+        Ex01 ex = new Ex01();
+        System.out.println(ex.i);
+        System.out.println(ex.c);
+    }
+}
