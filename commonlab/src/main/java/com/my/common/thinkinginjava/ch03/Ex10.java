66c48c0ecc71038d6a4295c9ff84dfacd1e4aab0
jlxuebailong <jlxuebailong@hotmail.com>
2019-05-02 07:58:47 +0800

    lab

--- commonlab/src/main/java/com/my/common/thinkinginjava/ch03/Ex10.java
+++ commonlab/src/main/java/com/my/common/thinkinginjava/ch03/Ex10.java
@@ -1,5 +1,8 @@
 package com.my.common.thinkinginjava.ch03;
 
+import java.util.Random;
+import static com.my.common.thinkinginjava.util.Print.*;
+
 /**
  * 位运算练习
  */
@@ -15,5 +18,22 @@ public class Ex10 {
         System.out.println(" x & y " + " = " + Integer.toBinaryString(x & y));
         System.out.println(" x | y " + " = " + Integer.toBinaryString(x | y));
         System.out.println(" x ^ y " + " = " + Integer.toBinaryString(x ^ y));
+
+        Random rand = new Random(47);
+        int i = rand.nextInt();
+        int j = rand.nextInt();
+        printBinaryInt("-1", -1);
+        printBinaryInt("1", 1);
+        printBinaryInt("i", i);
+        printBinaryInt("j", j);
+    }
+
+    static void printBinaryInt(String s, int i){
+        println(s + ", int: " + i + ", binary: \n " +
+        Integer.toBinaryString(i));
+    }
+    static void printBinaryLong(String s, long l){
+        println(s + ", long: " + l + ", binary: \n " +
+                Long.toBinaryString(l));
     }
 }
--- commonlab/src/main/java/com/my/common/thinkinginjava/ch04/Fibonacci.java
+++ commonlab/src/main/java/com/my/common/thinkinginjava/ch04/Fibonacci.java
@@ -1,18 +1,22 @@
 package com.my.common.thinkinginjava.ch04;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+
 import static com.my.common.thinkinginjava.util.Print.*;
 /**
  * 斐波那契数列
  */
 public class Fibonacci {
 
-    static void printFibonacci(int steps){
+    static int[] printFibonacci(int steps){
         int[] array = new int[ steps ];
         array[0] = array[1] = 1;
-
         for(int i = 2; i < steps; i++){
             array[i] = array[ i - 2 ] + array[ i - 1 ];
         }
+        return array;
+    }
 
+    static void printArray(int[] array){
         for(int i : array) {
             print(i);
             print(" ");
@@ -20,6 +24,6 @@ public class Fibonacci {
     }
 
     public static void main(String[] args){
-        printFibonacci(15);
+        printArray(printFibonacci(15));
     }
 }
